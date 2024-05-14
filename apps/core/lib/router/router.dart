182ca99e2686e5c8155f1a33d03d26f182a754ae
hiranya <hiranya2103@gmail.com>
2024-05-14 18:23:56 +0700

    feat: implement core router [AQ-1013]

---
+++ apps/core/lib/router/router.dart
@@ -0,0 +1,29 @@
+import 'package:core/layouts/navbar.dart';
+import 'package:go_router/go_router.dart';
+import 'package:product/features/home/screen/home_screen.dart';
+import 'package:product/features/map/home/screen/map_screen.dart';
+
+final routes = ['home', 'maps', 'ranks'];
+
+final router = GoRouter(routes: [
+  ShellRoute(
+      pageBuilder: (context, state, child) {
+        return NoTransitionPage(
+            child: BottomNavigation(
+          location: state.matchedLocation,
+          child: child,
+        ));
+      },
+      routes: [
+        GoRoute(
+          path: '/',
+          name: 'home',
+          builder: (context, state) => const HomeScreen(title: 'Home Screen'),
+        ),
+        GoRoute(
+          path: '/maps',
+          name: 'maps',
+          builder: (context, state) => const MapScreen(title: 'Map Screen'),
+        ),
+      ]),
+]);
