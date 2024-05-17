f915a1c845ad0f7e5fa32b3fea6a3a626e69c037
phuriphon.s <phuriphon.s@orbitdigital.co.th>
2024-05-17 14:55:19 +0700

    feat: implement search in map. [AQ-1014]

--- apps/product/lib/features/map/screen/map_screen.dart
+++ apps/product/lib/features/map/screen/map_screen.dart
@@ -38,15 +38,17 @@ class _MapScreenState extends State<MapScreen> {
     _weatherService = getIt.get<WeatherProjectionService>();
     _weatherSearchService = getIt.get<WeatherProjectionService>();
     _fetchCurrentWeather();
-    fetchMarkerFromCity('Chiang Mai');
   }
 
   Future<void> _fetchCurrentWeather() async {
     try {
       final weatherData = await _weatherService.getCurrentLocationWeather();
+
       setState(() {
         _currentWeather = weatherData;
+        fetchMarkerFromCity(_currentWeather!.cityName);
       });
+
       if (_currentWeather?.cityGeo != null) {
         _currentWeather!.cityGeo;
       } else {
@@ -54,7 +56,6 @@ class _MapScreenState extends State<MapScreen> {
       }
     } catch (error) {
       setState(() {
-        // Handle error
       });
       _fetchSearchWeather('');
     }
@@ -87,11 +88,10 @@ class _MapScreenState extends State<MapScreen> {
   }
 
   bool _isMarkerInRange(String pm25) {
-    // Always return true when 'No filter' is selected
     if (_dropDownMenu == 'No filter') {
       return true;
     }
-    // Check the range for other filter options
+
     int value = int.tryParse(pm25) ?? 0;
     if (_dropDownMenu == '0-50' && value >= 0 && value <= 50) {
       return true;
@@ -107,7 +107,6 @@ class _MapScreenState extends State<MapScreen> {
 
   Color getColorForPm25(String pm25) {
     int value = int.tryParse(pm25) ?? 0;
-    // Return color based on PM2.5 value even when 'No filter' is selected
     if (_dropDownMenu == '0-50' && value >= 0 && value <= 50) {
       return Colors.green;
     } else if (_dropDownMenu == '51-100' && value >= 51 && value <= 100) {
@@ -117,7 +116,7 @@ class _MapScreenState extends State<MapScreen> {
     } else if (_dropDownMenu == '<200' && value > 200) {
       return Colors.purpleAccent;
     }
-    // For 'No filter', return colors based on the PM2.5 value
+
     if (value >= 0 && value <= 50) {
       return Colors.green;
     } else if (value >= 51 && value <= 100) {
@@ -127,10 +126,8 @@ class _MapScreenState extends State<MapScreen> {
     } else if (value > 200) {
       return Colors.purpleAccent;
     }
-    return Colors.transparent; // Default color
+    return Colors.transparent;
   }
-
-
 
   Future<void> fetchMarkerFromCity(String city) async {
     try {
@@ -143,14 +140,14 @@ class _MapScreenState extends State<MapScreen> {
         return isWithinPastYear && hasAqiData && _isMarkerInRange(data.aqi ?? '');
       }).toList() ?? [];
 
-      markers.clear();
+      List<Marker> newMarkers = [];
       for (var data in filteredData) {
         final List<double> geo = data.geo ?? [];
         final LatLng coordinates = LatLng(geo[0], geo[1]);
         final pm25 = data.aqi ?? '-';
         final stationName = data.station?.name ?? '';
         print('Adding marker with AQI: $pm25');
-        markers.add(
+        newMarkers.add(
           Marker(
             point: coordinates,
             width: 40,
@@ -193,8 +190,9 @@ class _MapScreenState extends State<MapScreen> {
           ),
         );
       }
+
       setState(() {
-        fetchMarkerFromCity(city);
+        markers = newMarkers;
       });
     } catch (error) {
       print("Error fetching markers: $error");
@@ -228,7 +226,9 @@ class _MapScreenState extends State<MapScreen> {
           FlutterMap(
             options: MapOptions(
               initialCenter: _currentWeather!.cityGeo,
-              initialZoom: 5,
+              initialZoom: 12,
+              minZoom: 3,
+              maxZoom: 18,
             ),
             children: [
               TileLayer(
@@ -289,6 +289,7 @@ class _MapScreenState extends State<MapScreen> {
                     setState(() {
                       _dropDownMenu = newValue!;
                     });
+                    fetchMarkerFromCity(_currentWeather!.cityName);
                   },
                   value: _dropDownMenu,
                   underline: Container(),
